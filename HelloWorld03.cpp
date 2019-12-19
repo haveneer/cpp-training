34ba87ebe6194a03867810db7e7353ab2eb06700
Pascal Havé <hpwxf@haveneer.com>
2019-12-19 11:26:44 +0100

    Add HelloWorld03 example

---
+++ HelloWorld03.cpp
@@ -0,0 +1,14 @@
+/*
+ * Trivial example
+ * No copyright © 2020
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+  // Print message on standard output
+  std::cout << "Hello, world!" << std::endl;
+  return 0;
+}
+     
