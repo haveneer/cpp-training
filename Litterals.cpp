52abcbfeb19ad4fcd970ad30d48ec007b78f0eac
Pascal Havé <hpwxf@haveneer.com>
2019-12-21 15:50:07 +0100

    Add Todo

---
+++ Litterals.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+/* standard initialization
+
+int a = 1; // conversion
+bool b = 2;
+
+  uniform initialization
+
+int a { 1 };
+
+bool b { true };
+
+*/
+
+}
