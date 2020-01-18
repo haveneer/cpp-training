b2b1376152537a5d315d2e8a0da09cb85d60c882
Pascal Havé <hpwxf@haveneer.com>
2020-01-18 17:49:25 +0100

    Add fragment's for progressive display

--- Enum.cpp
+++ Enum.cpp
@@ -2,26 +2,20 @@
 #include <iostream>
 //#endregion
 
-// Unscoped enum
-enum Color {
-  Blue,
-  Green,
-  Red
-};
-
-enum class Suit {
-  Heart,
-  Spade,
-  Diamond,
-  Club
-};
 
 int main() {
+  //#region [Unscoped enum]
+  enum Color { Blue, Green, Red };
+
   Color color = Blue; // NOTA: Blue value has no prefix !
   std::cout << "color is " << color << '\n';
   // color = 2; // even if it is encoded on a integral type
   //            // you cannot assign an integer to it
 
+  //#endregion
+
+  //#region [Scoped enum]
+  enum class Suit { Heart, Spade, Diamond, Club };
   Suit suit = Suit::Diamond; // NOTA: with a scope prefix !
 
   // error: invalid operands to binary expression
@@ -29,4 +23,5 @@ int main() {
 
   // with an explicit cast, it could be ok: do it carefully
   std::cout << "suit is " << static_cast<int>(suit) << '\n';
+  //#endregion
 }
