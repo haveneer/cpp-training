b43426a711b2968b8827457b36ad5fe591bb40d9
Pascal Havé <hpwxf@haveneer.com>
2020-04-04 12:37:03 +0200

    Fix MSVC compile error (don't like (int[]){...} )

--- HelloWorld14.cpp
+++ HelloWorld14.cpp
@@ -2,11 +2,12 @@
 
 template<typename Arg, typename... Args>
 void print(Arg&& arg, Args&& ...args){
-    std::cout << std::forward<Arg>(arg);
-    (void)(int[]){0, (void(std::cout << ',' << std::forward<Args>(args)), 0)...};
-    std::cout << std::endl;
+  std::cout << std::forward<Arg>(arg);
+  int _[]{0, (void(std::cout << ',' << std::forward<Args>(args)), 0)...};
+  (void)_; // disable warning on unused variable 
+  std::cout << std::endl;
 };
 
 int main() { // C++11 / C++14
-    print("Hello", "world!");
+  print("Hello", "world!");
 }
--- HelloWorld17.cpp
+++ HelloWorld17.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-auto print =[](auto&& arg, auto&& ...args){
-    std::cout << std::forward<decltype(arg)>(arg);
-    ((std::cout << ", " << std::forward<decltype(args)>(args)), ...);
-    std::cout << std::endl;
+auto print = [](auto &&arg, auto &&... args) {
+  std::cout << std::forward<decltype(arg)>(arg);
+  ((std::cout << ", " << std::forward<decltype(args)>(args)), ...);
+  std::cout << std::endl;
 };
 
 int main() { // C++17
-    print("Hello", "world!");
+  print("Hello", "world!");
 }
