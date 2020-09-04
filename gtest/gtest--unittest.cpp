ce362601d377975d986d504c82c4d4d0205def14
Pascal Havé <hpwxf@haveneer.com>
2020-09-04 09:45:43 +0200

    Move all gtest tests in gtest directory

---
+++ gtest/gtest--unittest.cpp
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+#include <cmath>
+#include <iostream>
+
+TEST(MyTest, Feature1) {
+  EXPECT_EQ(1, 2); // Non fatal error: continue
+  ASSERT_EQ(1, 2); // fatal error: stop
+  EXPECT_EQ(1, 2); // never executed
+}
+
+// Disbaled by name
+TEST(DISABLED_MyTest, Feature2) { ASSERT_EQ(1, 2); }
+TEST(MyTest, DISABLED_Feature3) { ASSERT_EQ(1, 2); }
+
+TEST(MyTest, Feature4) {
+  ASSERT_EQ(1, 2);
+  GTEST_SKIP(); // modern disable style from 1.10.0
+  ASSERT_EQ(3, 4);
+}
+
+double square_root(double num) {
+  if (num < 0.0) {
+    std::cerr << "Error: Negative Input\n";
+    exit(1);
+  }
+  return std::sqrt(num);
+}
+
+TEST(MyTest, SquareRoot) {
+  EXPECT_DOUBLE_EQ(square_root(4), 2 + 1e-14);  // exact floating point match
+  EXPECT_NEAR(square_root(4), 2 + 1e-14, 1e-8); // exact floating point match
+  EXPECT_EXIT(square_root(-4), ::testing::ExitedWithCode(1),
+              "Error: Negative Input"); // parse std::cerr using regex 
+}
+
+
+
+
+
+// This is the default main.
+// If not defined gtest_main will do it for you.
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
