40478c5ceac6a64b2a80c32304ae75f7f661a49f
Pascal Havé <hpwxf@haveneer.com>
2020-09-17 09:56:38 +0200

    Add exotic code using 'dark' features of C++

---
+++ Curiosity/exotic_obfuscated_cpp.cpp
@@ -0,0 +1,10 @@
+/* This is valid C++ */
+auto main() -> decltype('O.o') try
+<%[O_O = 0b0]<%
+    https://gha.st/a-tour-of-rare-cpp-features/
+    typedef struct o O;
+    o*(*((&&o(o*o))<:'o':>))(o*o);
+    if(O*O = decltype(0'0[o(0)](0))(0)) 1,000.00;
+    else return 0==O==0 ? throw O_O : O_O;
+  %>();%>
+catch(...) { throw; }
