0b9112075ca7f1f985b842d65d861a2df05090a1
Pascal Havé <hpwxf@haveneer.com>
2020-09-26 20:23:14 +0200

    Add more tests

--- Solutions/dicho_project/dicho_tests.cpp
+++ Solutions/dicho_project/dicho_tests.cpp
@@ -11,20 +11,31 @@ TEST(dicho, Success) {
   Real eps = 1e-9;
 
   int n = 0;
-  ASSERT_NO_THROW(dicho(a, b, eps, &n));
+  EXPECT_NO_THROW(dicho(a, b, eps, &n));
   EXPECT_GT(eps, std::abs(b - a));
   EXPECT_NEAR(x0, (a + b) / 2, x0 * eps);
   EXPECT_LT(0, n);
   EXPECT_GT(100, n);
 }
 
-TEST(dicho, InvalidRange) { ASSERT_THROW(dicho(2, 1), std::invalid_argument); }
+TEST(dicho, InvalidRange) {
+  x0 = 1; // global
+
+  EXPECT_THROW(dicho(2, 1), std::invalid_argument);
+
+  Real a = x0 / 2;
+  Real b = 2 * x0;
+  Real eps = 1e-9;
+  int n = -1; // magic value; should not change if throws
+  EXPECT_THROW(dicho(b, a, eps, &n), std::invalid_argument);
+  EXPECT_EQ(-1, n);
+}
 
 TEST(dicho, InvalidEpsilon) {
   x0 = 1; // global
   Real a = x0 / 2;
   Real b = 2 * x0;
-  ASSERT_THROW(dicho(a, b, -1, nullptr), std::invalid_argument);
+  EXPECT_THROW(dicho(a, b, -1, nullptr), std::invalid_argument);
 }
 
 TEST(dicho, BadConverge) {
