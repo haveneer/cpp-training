77f88f7eac45c67c3b451cee59cad26db08dd9ce
Pascal HAVÉ <hpwxf@haveneer.com>
2021-03-25 14:20:28 +0100

    Update parametric test format

--- gtest/gtest-parametric--unittest.cpp
+++ gtest/gtest-parametric--unittest.cpp
@@ -3,17 +3,18 @@
 // include GTest tools
 #include "gtest/gtest.h"
 #include <tuple>
-//#endregion
 
 #include "gtest-parametric-permutation.hpp"
 #include "gtest-parametric-sequence.hpp"
 #include "gtest-triangle.hpp"
+//#endregion
 
-// parametrized TestSuit level fixture
+// Parametrized TestSuit level fixture
 class MyParametrizedTest
     : public ::testing::TestWithParam<std::tuple<int, int, int>> {
-protected:                // needs to be accessible by sub-classes (e.g. TEST*)
-  void SetUp() override { // don't forget 'override' (TODO why ?)
+  //#region [Fixture details]
+protected: // needs to be accessible by sub-classes (e.g. TEST*)
+  void SetUp() override {
     // Retrieve info about running test
     const testing::TestInfo *const test_info =
         testing::UnitTest::GetInstance()->current_test_info();
@@ -21,6 +22,7 @@ protected:                // needs to be accessible by sub-classes (e.g. TEST*)
       std::cout << test_info->test_suite_name() << "." << test_info->name() << "\n";
   }
   static inline bool m_verbose = true;
+  //#endregion
 };
 
 using TriangleSuccessTestSuit = MyParametrizedTest;
@@ -30,8 +32,8 @@ TEST_P(TriangleSuccessTestSuit, Job) {
 }
 
 INSTANTIATE_TEST_SUITE_P(Part1, TriangleSuccessTestSuit,
-                         testing::Values(std::make_tuple(2, 3, 4),
-                                         std::make_tuple(3, 3, 3),
+                         testing::Values(std::make_tuple(2, 3, 4), // List of
+                                         std::make_tuple(3, 3, 3), // fixed values
                                          std::make_tuple(3, 3, 5),
                                          std::make_tuple(5, 3, 3),
                                          std::make_tuple(3, 5, 3)));
@@ -44,11 +46,12 @@ TEST_P(TriangleFailureTestSuit1, Job) {
 
 std::vector<int> other_values = {2, 5, 0, -1};
 INSTANTIATE_TEST_SUITE_P(Part2, TriangleFailureTestSuit1,
-                         testing::Combine(testing::Values(1), testing::Values(3),
-                                          testing::ValuesIn(other_values)));
+                         testing::Combine(testing::Values(1),      // Fixed values
+                                          testing::Range(1, 8, 3), // Range of values
+                                          testing::ValuesIn(other_values) // List
+                                          ));
 
-/////////////////////////////////////////////////////
-
+//#region [Custom generator]
 using TriangleFailureTestSuit2 = MyParametrizedTest;
 TEST_P(TriangleFailureTestSuit2, Job) {
   auto [j, k, l] = GetParam();
@@ -66,3 +69,4 @@ INSTANTIATE_TEST_SUITE_P(Part3, TriangleFailureTestSuit2,
                                  testing::Values(std::make_tuple(1, 3, 5))),
                              testing::Values(std::make_tuple(0, 0, 0)),
                          }));
+//#endregion
