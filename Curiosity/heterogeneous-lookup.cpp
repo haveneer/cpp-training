5055adebd4a7ce3f30b749305a3768b5e63706fa
Pascal HAVÉ <hpwxf@haveneer.com>
2021-05-16 21:02:05 +0200

    Update heterogeneous-lookup.cpp using a custom comparator

--- Curiosity/heterogeneous-lookup.cpp
+++ Curiosity/heterogeneous-lookup.cpp
@@ -1,7 +1,7 @@
+#include <cassert>
 #include <set>
 #include <string>
 #include <string_view>
-#include <cassert>
 
 struct Product {
   std::string name;
@@ -20,6 +20,19 @@ bool operator<(const std::string_view &name1, const Product &p2) {
   return name1 < p2.name;
 }
 
+struct MyComparator { // my custom comparator compatible with heterogeneous lookup
+  bool operator()(const Product &p1, const Product &p2) const {
+    return p1.name < p2.name;
+  }
+  bool operator()(const Product &p1, const std::string_view &name2) const {
+    return p1.name < name2;
+  }
+  bool operator()(const std::string_view &name1, const Product &p2) const {
+    return name1 < p2.name;
+  }
+  using is_transparent = int; // enable heterogeneous lookup
+};
+
 int main() {
   std::set<Product> products1{
       {"Car", "This is a super car that costs a lot", 100'000.0},
@@ -30,11 +43,22 @@ int main() {
   // default comparator
 
   std::set<Product, std::less<>> products2{
-      // use C++14 heterogenous comparator
+      // use C++14 heterogeneous comparator
       {"Car", "This is a super car that costs a lot", 100'000.0},
       {"Ball", "A cheap but nice-looking ball to play", 100.0},
       {"Orange", "Something to eat and refresh", 50.0}};
 
-  auto finder = products2.find("Car"); // Cannot compare Product and 'const char *'
-  assert(finder != products2.end());
+  auto finder2 = products2.find("Car"); // Can compare Product and 'const char *'
+                                        // this 'find' method is not the same
+                                        // it use a template type + SFINAE
+                                        // see std::less<void> + is_transparent
+  assert(finder2 != products2.end());
+
+  std::set<Product, MyComparator> products3{
+      {"Car", "This is a super car that costs a lot", 100'000.0},
+      {"Ball", "A cheap but nice-looking ball to play", 100.0},
+      {"Orange", "Something to eat and refresh", 50.0}};
+
+  auto finder3 = products3.find("Car"); // My custom 'transparent' comparator
+  assert(finder3 != products2.end());
 }
